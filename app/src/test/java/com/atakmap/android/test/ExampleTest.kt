f887cb6100cfabef784bb0554590d244edd4883f
zraza-riis <zraza@riis.com>
2025-08-04 13:43:22 -0400

    initial mav sdk commit

---
+++ app/src/test/java/com/atakmap/android/test/ExampleTest.kt
@@ -0,0 +1,11 @@
+package com.atakmap.android.test
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class ExampleTest {
+    @Test
+    fun basicArithmetic() {
+        assertEquals(4, 2 + 2)
+    }
+}
